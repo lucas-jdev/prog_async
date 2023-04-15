60c2cceb3529aff9c1f129b7774fb7d22d489545
Lucas Jdev <lucas.jdev1@gmail.com>
2023-04-15 08:20:13 -0300

    up quality unit tests client

--- src/test/java/dev/codelevel/prog_async/unitario/dominio/ClienteTest.java
+++ src/test/java/dev/codelevel/prog_async/unitario/dominio/ClienteTest.java
@@ -3,6 +3,8 @@ package dev.codelevel.prog_async.unitario.dominio;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,60 +16,68 @@ import dev.codelevel.prog_async.dominio.cliente.excecoes.ExcecaoNomeInvalido;
 import dev.codelevel.prog_async.dominio.cliente.modelo.Cliente;
 import dev.codelevel.prog_async.dominio.cliente.modelo.Cliente.DadosCliente;
 
+@DisplayName("Testes de unidade da classe Cliente")
 class ClienteTest {
 
     private final String NOME = "Lucas";
     private final String CPF = "111.111.111-11";
     private final int IDADE = 20;
 
-    // regras de negócio
+    @Nested
+    @DisplayName("Testes das regras de negócio")
+    class RegraDeNegocio{
+        
+        @ParameterizedTest
+        @CsvSource({
+                "null",
+                "''",
+                "' '"
+        })
+        @DisplayName("Deve lançar ExcecaoCPFInvalido para CPF inválido")
+        void cpfInvalidoDeveRetornarExcecao(String cpf) {
+            cpf = validaSeNullEstaEmTexto(cpf);
+            DadosCliente dados = Cliente.entradaDadosCliente(NOME, cpf, IDADE);
+            assertThrows(ExcecaoCPFInvalido.class, () -> Cliente.criarCliente(dados));
+        }
 
-    @ParameterizedTest
-    @CsvSource({
-            "null",
-            "''",
-            "' '"
-    })
-    void cpfInvalidoDeveRetornarExcecao(String cpf) {
-        cpf = validaSeNullEstaEmTexto(cpf);
-        DadosCliente dados = Cliente.entradaDadosCliente(NOME, cpf, IDADE);
-        assertThrows(ExcecaoCPFInvalido.class, () -> Cliente.criarCliente(dados));
+        @ParameterizedTest
+        @CsvSource({
+                "null",
+                "''",
+                "' '"
+        })
+        @DisplayName("Deve lançar ExcecaoNomeInvalido para nome inválido")
+        void nomeInvalidoDeveRetornarExcecao(String nome) {
+            nome = validaSeNullEstaEmTexto(nome);
+            DadosCliente dados = Cliente.entradaDadosCliente(nome, CPF, IDADE);
+            assertThrows(ExcecaoNomeInvalido.class, () -> Cliente.criarCliente(dados));
+        }
+    
+        @ParameterizedTest
+        @ValueSource(ints = { 15, 16, 17 })
+        @DisplayName("Deve lançar ExcecaoIdadeInvalida para idade inválida")
+        void menorDeIdade(int idade) {
+            DadosCliente dados = Cliente.entradaDadosCliente(NOME, CPF, idade);
+            assertThrows(ExcecaoIdadeInvalida.class, () -> Cliente.criarCliente(dados));
+        }
+    
+        @ParameterizedTest
+        @ValueSource(ints = { 18, 19, 20 })
+        @DisplayName("Deve criar cliente para idade válida")
+        void maiorDeIdade(int idade) {
+            DadosCliente dados = Cliente.entradaDadosCliente(NOME, CPF, idade);
+            Cliente cliente = Cliente.criarCliente(dados);
+            assertNotNull(cliente);
+        }
+
+        private String validaSeNullEstaEmTexto(String valor) {
+            return valor.equals("null") ? null : valor;
+        }
+
     }
-
-    private String validaSeNullEstaEmTexto(String valor) {
-        return valor.equals("null") ? null : valor;
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "null",
-            "''",
-            "' '"
-    })
-    void nomeInvalidoDeveRetornarExcecao(String nome) {
-        nome = validaSeNullEstaEmTexto(nome);
-        DadosCliente dados = Cliente.entradaDadosCliente(nome, CPF, IDADE);
-        assertThrows(ExcecaoNomeInvalido.class, () -> Cliente.criarCliente(dados));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = { 15, 16, 17 })
-    void menorDeIdade(int idade) {
-        DadosCliente dados = Cliente.entradaDadosCliente(NOME, CPF, idade);
-        assertThrows(ExcecaoIdadeInvalida.class, () -> Cliente.criarCliente(dados));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = { 18, 19, 20 })
-    void maiorDeIdade(int idade) {
-        DadosCliente dados = Cliente.entradaDadosCliente(NOME, CPF, idade);
-        Cliente cliente = Cliente.criarCliente(dados);
-        assertNotNull(cliente);
-    }
-
-    // testes de unidade
 
     @Test
+    @DisplayName("Deve criar cliente")
     void testCriarCliente() {
         DadosCliente dadosCliente = Cliente.entradaDadosCliente(NOME, CPF, IDADE);
         Cliente cliente = Cliente.criarCliente(dadosCliente);
@@ -76,12 +86,14 @@ class ClienteTest {
     }
 
     @Test
+    @DisplayName("Deve criar dados do cliente")
     void testEntradaDadosCliente() {
         DadosCliente dadosCliente = Cliente.entradaDadosCliente(NOME, CPF, IDADE);
         assertNotNull(dadosCliente);
     }
 
     @Test
+    @DisplayName("Deve retornar o estado do cliente em String")
     void testToString() {
         DadosCliente dadosCliente = Cliente.entradaDadosCliente(NOME, CPF, IDADE);
         Cliente cliente = Cliente.criarCliente(dadosCliente);
